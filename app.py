96549e362a570c43e932318ef07d057f36752e14
user1 <rrr9288@nyu.edu>
2024-05-10 15:18:44 -0400

    c

--- app.py
+++ app.py
@@ -22,9 +22,28 @@ def home():
 def index():
     return render_template('index.html')
 
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    selected_option = request.form['selected_option']
 
+    # Define queries based on the selected option
+    if selected_option == '1':
+        return("you chose 1")
+    elif selected_option == '2':
+        #sql_query = "SELECT * FROM table2"
+        return("you chose 2")
+    elif selected_option == '3':
+        #sql_query = "SELECT * FROM table3"
+        return("you chose 3")
+    else:
+        # Handle default case or return an error
+        return "Invalid option selected."
 
+    #cursor.execute(sql_query)
+    #result = cursor.fetchall()
 
+    # Pass the result to the result.html template
+    return render_template('result.html' #result=result)
 
 if __name__ == "__main__":
     app.run(debug = True)
