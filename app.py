aac124a56af30c6ea630cd88ea2eb9fa6df0af33
user1 <rrr9288@nyu.edu>
2024-05-10 14:53:26 -0400

    c

--- app.py
+++ app.py
@@ -18,5 +18,13 @@ cursor = mydb.cursor()
 def home():
     return render_template('base.html')
 
+@app.route('/index')
+def index()
+    return render_template('index.html')
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug = True)
