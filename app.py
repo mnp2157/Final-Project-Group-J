f43b9ecaf2fd050760ea9407057e7fb71133bf03
user1 <rrr9288@nyu.edu>
2024-05-10 14:42:53 -0400

    c

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask
 import mysql.connector
+from flask import render_template, request, redirect, url_for, make_response
+import mysql.connector
+# instantiate the app
+app= Flask(__name__)
 
 # Create connection object
 mydb = mysql.connector.connect(
@@ -10,4 +14,9 @@ mydb = mysql.connector.connect(
 )
 # Create cursor
 cursor = mydb.cursor()
+@app.route('/')
+def home():
+    return render_template('base.html')
 
+if __name__ == "__main__":
+    app.run(debug = True)
